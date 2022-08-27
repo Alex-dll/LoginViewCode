b70b00313e5163e846f340791428892cbf19bd96
alex-dll <alexsandrojr.303@gmail.com>
2022-08-27 12:49:15 -0300

    feat: making a new screen

---
+++ LoginViewCode/Register/VC/RegisterVC.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterVC.swift
+//  LoginViewCode
+//
+//  Created by Alex on 27/08/22.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+
+}
