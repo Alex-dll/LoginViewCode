b9ebcdb96bb5e186c71999fb50f630847f07bdb5
alex-dll <alexsandrojr.303@gmail.com>
2022-08-27 09:09:50 -0300

    feat: add first label

--- LoginViewCode/ViewController.swift
+++ LoginViewCode/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var loginLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
+        self.view.addSubview(self.loginLabel)
+        self.setUpContraintes() 
     }
-
+    
+    private func setUpContraintes(){
+        
+        NSLayoutConstraint.activate([
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.loginLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 
 }
 
